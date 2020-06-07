a51dec70b9de04463ac6888b3a1170806292c2be
MarcinGorcz <61966094+MarcinGorcz@users.noreply.github.com>
2020-06-07 20:33:07 +0200

    nazwy plikow na bardziej pytoniczne

--- FinalProject1.py
+++ FinalProject1.py
@@ -14,83 +14,92 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg, NavigationTool
 
 #3D:
 #Początkowy zestaw punktow do Voronoi3D:
-sizeOfDiagram = 10
-pointsFromFile = [0, 0, 0]
-pointsDefault = [[0, 0, 0], [sizeOfDiagram - 1, sizeOfDiagram - 1, sizeOfDiagram - 1], [sizeOfDiagram - 1, 0, 0]]
+size_of_diagram = 10
+points_from_file = [0, 0, 0]
+points_default = [[0, 0, 0], [size_of_diagram - 1, size_of_diagram - 1, size_of_diagram - 1], [size_of_diagram - 1, 0, 0]]
+
 
 def Voronoi3D(points):
-    resultTable = np.zeros([sizeOfDiagram, sizeOfDiagram, sizeOfDiagram], np.int)
+    result_table = np.zeros([size_of_diagram, size_of_diagram, size_of_diagram], np.int)
 
-    for tableIndex, table in enumerate(resultTable):
-        for rowIndex, row in enumerate(table):
-            for columnIndex, column in enumerate(row):
-                currentDistance = sizeOfDiagram * sizeOfDiagram * sizeOfDiagram
-                currentPoint = "X"
-                for indexOfVoxel, currentVoxel in enumerate(points):
-                    distanceFromCurrentVoxelToPoint = distance3d(tableIndex, rowIndex, columnIndex, currentVoxel[0], currentVoxel[1], currentVoxel[2])
-                    if currentDistance > distanceFromCurrentVoxelToPoint:
-                        currentDistance = distanceFromCurrentVoxelToPoint
-                        currentPoint = indexOfVoxel+1
-                resultTable[tableIndex][rowIndex][columnIndex] = currentPoint
-    return resultTable
+    for table_index, table in enumerate(result_table):
+        for row_index, row in enumerate(table):
+            for column_index, column in enumerate(row):
+                current_distance = size_of_diagram * size_of_diagram * size_of_diagram
+                current_point = "X"
+                for index_of_voxel, current_voxel in enumerate(points):
+                    distance_from_current_voxel_to_point = distance3d(table_index, row_index, column_index, current_voxel[0], current_voxel[1], current_voxel[2])
+                    if current_distance > distance_from_current_voxel_to_point:
+                        current_distance = distance_from_current_voxel_to_point
+                        current_point = index_of_voxel+1
+                result_table[table_index][row_index][column_index] = current_point
+    return result_table
 
-def isValidIndexOfPoint(stringWithIndexesOfPoint):
-    for index in stringWithIndexesOfPoint:
+
+def is_valid_index_of_point(string_with_indexes_of_point):
+    for index in string_with_indexes_of_point:
         if not index.isdigit():
             return False
     return True
+
 
 def distance3d(x1, y1, z1, x2, y2, z2):
     distance = sqrt((x1-x2)**2+(y1-y2)**2+(z1-z2)**2)
     return distance
 
-def LoadFile():
-    global pointsFromFile
-    localListOfPoints = np.empty((0,3), int)
-    filename = fd.askopenfilename(initialdir=".", title="Select file",filetype=(("Text file","*.txt"),("All files","*.*")) )
+
+def load_file():
+    global points_from_file
+    local_list_of_points = np.empty((0,3), int)
+    filename = fd.askopenfilename(initialdir=".", title="Select file", filetype=(("Text file", "*.txt"), ("All files", "*.*")))
     if filename:
         with open(filename,"r") as f:
-            contentOfFileOfPoints = "".join(f.read().split())
-            stringFromListOfPoints = contentOfFileOfPoints.split(";")
-            for point in stringFromListOfPoints:
-                indexOfPoint = point.split(",")
-                if (isValidIndexOfPoint(indexOfPoint)):
-                    localListOfPoints = np.append(localListOfPoints,np.array([list(map(int, indexOfPoint))]),axis=0)
+            content_of_file_of_points = "".join(f.read().split())
+            string_from_list_of_points = content_of_file_of_points.split(";")
+            for point in string_from_list_of_points:
+                index_of_point = point.split(",")
+                if is_valid_index_of_point(index_of_point):
+                    local_list_of_points = np.append(local_list_of_points, np.array([list(map(int, index_of_point))]), axis=0)
                 else:
                     continue
-        pointsFromFile = localListOfPoints
+        points_from_file = local_list_of_points
 
-def RecalculateForNewSetOfPoints():
-    DrawVoronoi3DDiagram(pointsFromFile)
 
-def TableForPoints(points):
-    tableOfPoints = np.zeros([sizeOfDiagram, sizeOfDiagram, sizeOfDiagram], np.int)
+def recalculate_for_new_set_of_points():
+    draw_voronoi3d_diagram(points_from_file)
+
+
+def table_for_points(points):
+    table_of_points = np.zeros([size_of_diagram, size_of_diagram, size_of_diagram], np.int)
     for point in points:
-        tableOfPoints[[point[0]],[point[1]],[point[2]]]=True
-    return tableOfPoints
+        table_of_points[[point[0]], [point[1]], [point[2]]] = True
+    return table_of_points
 
-def TableOfAreasForPoints(punkty, VoronoiTable):
-    listOfPoints = list(range(1, np.shape(punkty)[0]+1))
-    listOfAreaMatrix = []
-    for indexOfPoint in listOfPoints:
-        areaMatrixForOneColor = np.zeros(VoronoiTable.shape, np.int)
-        areaMatrixForOneColor[np.nonzero(VoronoiTable == indexOfPoint)] = 1
-        listOfAreaMatrix.append([indexOfPoint, areaMatrixForOneColor])
-    return listOfAreaMatrix
 
-def DrawVoronoi3DDiagram(points):
+def table_of_areas_for_points(list_of_points, voronoi_table):
+    list_of_points = list(range(1, np.shape(list_of_points)[0] + 1))
+    list_of_area_matrix = []
+    for indexOfPoint in list_of_points:
+        area_matrix_for_one_color = np.zeros(voronoi_table.shape, np.int)
+        area_matrix_for_one_color[np.nonzero(voronoi_table == indexOfPoint)] = 1
+        list_of_area_matrix.append([indexOfPoint, area_matrix_for_one_color])
+    return list_of_area_matrix
+
+
+def draw_voronoi3d_diagram(points):
     fig = Figure(figsize=(5, 4), dpi=100)
     canvas = FigureCanvasTkAgg(fig, master=root)
     canvas.draw()
     ax = fig.add_subplot(1, 1, 1, projection="3d")
-    for area in TableOfAreasForPoints(points, Voronoi3D(points)):
-        randomColorHex = '#' + format(random.randint(17, 255), 'x') + format(random.randint(17, 255), 'x') + format(random.randint(17, 255), 'x') +"30"
-        #print(randomColorHex)
-        ax.voxels(area[1], facecolors=randomColorHex, edgecolors=None, shade=False)
-    ax.voxels(TableForPoints(points), facecolor='black', edgecolors='black', shade=False)
+    for area in table_of_areas_for_points(points, Voronoi3D(points)):
+        random_color_hex = '#' + format(random.randint(17, 255), 'x') + format(random.randint(17, 255), 'x') + format(random.randint(17, 255), 'x') + "30"
+        #print(random_color_hex)
+        ax.voxels(area[1], facecolors=random_color_hex, edgecolors=None, shade=False)
+    ax.voxels(table_for_points(points), facecolor='black', edgecolors='black', shade=False)
 
     canvas.get_tk_widget().place(x=150, y=5)
     return canvas
+
 
 #Ogolne
 root = tk.Tk()
@@ -99,15 +108,15 @@ root.wm_title("Voronoi3D")
 root.resizable(False, False)
 
 #Definiowanie elementow GUI
-LoadButton = tk.Button(root, text="Wczytaj punkty z pliku", command=LoadFile, height=1, width=18)
-RecalculateButton = tk.Button(root, text="Przelicz", command=RecalculateForNewSetOfPoints, height=1, width=18)
+load_button = tk.Button(root, text="Wczytaj punkty z pliku", command=load_file, height=1, width=18)
+recalculate_button = tk.Button(root, text="Przelicz", command=recalculate_for_new_set_of_points, height=1, width=18)
 
 #Rysowanie wykresu 3D Voronoi
-canvas = DrawVoronoi3DDiagram(pointsDefault)
+canvas = draw_voronoi3d_diagram(points_default)
 toolbar = NavigationToolbar2Tk(canvas, root)
 
 #Pozycjonowanie elementow GUI
-LoadButton.place(x = 5, y = 5)
-RecalculateButton.place(x = 5, y = 35)
+load_button.place(x=5, y=5)
+recalculate_button.place(x=5, y=35)
 toolbar.update()
 root.mainloop()
